0252dfe00fe75871709efd21b500ac89507d8aa5
Riccardo Bertolini <info@riccardobertolini.net>
2023-01-15 22:33:59 +0100

    feat: updating socket endpoint

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,4 +1,4 @@
-import { Server, ServerOptions } from 'socket.io'
+import { Server } from 'socket.io'
 
 const SocketHandler = (
 	req: any,
@@ -20,61 +20,45 @@ const SocketHandler = (
 	io.on('connection', (socket) => {
 		console.log(`User Connected :${socket.id}`)
 
-		// Triggered when a peer hits the join room button.
-
 		socket.on('join', (roomName) => {
 			const { rooms } = io.sockets.adapter
 
 			const room = rooms.get(roomName)
-
-			// room == undefined when no such room exists.
 
 			if (room === undefined) {
 				socket.join(roomName)
 
 				socket.emit('created')
 			} else if (room.size === 1) {
-				// room.size == 1 when one person is inside the room.
-
 				socket.join(roomName)
 
 				socket.emit('joined')
 			} else {
-				// when there are already two people inside the room.
-
 				socket.emit('full')
 			}
 
 			console.log(rooms)
 		})
 
-		// Triggered when the person who joined the room is ready to communicate.
-
 		socket.on('ready', (roomName) => {
-			socket.broadcast.to(roomName).emit('ready') // Informs the other  peer in the room.
+			socket.broadcast.to(roomName).emit('ready')
 		})
-
-		// Triggered when server gets an icecandidate from a peer in the room.
 
 		socket.on(
 			'ice-candidate',
 			(candidate: RTCIceCandidate, roomName: string) => {
 				console.log(candidate)
 
-				socket.broadcast.to(roomName).emit('ice-candidate', candidate) // Sends Candidate to the other peer in the room.
+				socket.broadcast.to(roomName).emit('ice-candidate', candidate)
 			}
 		)
 
-		// Triggered when server gets an offer from a peer in the room.
-
 		socket.on('offer', (offer, roomName) => {
-			socket.broadcast.to(roomName).emit('offer', offer) // Sends Offer to the other peer in the room.
+			socket.broadcast.to(roomName).emit('offer', offer)
 		})
 
-		// Triggered when server gets an answer from a peer in the room.
-
 		socket.on('answer', (answer, roomName) => {
-			socket.broadcast.to(roomName).emit('answer', answer) // Sends Answer to the other peer in the room.
+			socket.broadcast.to(roomName).emit('answer', answer)
 		})
 
 		socket.on('leave', (roomName) => {
